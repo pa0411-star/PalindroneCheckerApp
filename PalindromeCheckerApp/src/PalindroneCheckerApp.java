a8f09aafd23dd710a7980f9269af0fa58e7e5459
pa0411-star <pa0411@srmist.edu.in>
2026-03-06 10:00:45 +0530

    Prasanna Added UC10-InsensitiveSpaceIgnoredPalindrome

--- PalindromeCheckerApp/src/PalindroneCheckerApp.java
+++ PalindromeCheckerApp/src/PalindroneCheckerApp.java
@@ -2,29 +2,29 @@ public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "A man a plan a canal Panama";
 
-        boolean isPalindrome = check(input, 0, input.length() - 1);
+        // Step 1: Normalize string
+        String normalized = input
+                .toLowerCase()                 // convert to lowercase
+                .replaceAll("[^a-z0-9]", "");  // remove spaces & symbols
 
+        boolean isPalindrome = true;
+
+        // Step 2: Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) !=
+                    normalized.charAt(normalized.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-
-    // Recursive function
-    private static boolean check(String s, int start, int end) {
-
-        // Base condition: if start >= end, it's palindrome
-        if (start >= end) {
-            return true;
-        }
-
-        // If characters don't match
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call moving inward
-        return check(s, start + 1, end - 1);
     }
 }
 
